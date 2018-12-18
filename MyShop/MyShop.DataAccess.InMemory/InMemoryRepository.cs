7adaba8abb18dcb70b5eff99f67e403d3ce38ee4
Mikiee <abulag.michael@gmail.com>
2018-12-18 23:44:13 +0800

    Generics to be continue

---
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class InMemoryRepository<T>
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string className;
+
+        public InMemoryRepository()
+        {
+
+        }
+    }
+}
