f51f8c26c616b4465b2b17ce807e566b1ad20355
Mikiee <abulag.michael@gmail.com>
2018-12-24 11:04:10 +0800

    Complete BasketService and BasketService interface

---
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,23 @@
+﻿using MyShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IBasketService 
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+
+
+        void RemoveFromBasket(HttpContextBase httpContex, string itemId);
+      
+
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+    }
+}
